bab3de5e9eeacd0d842af5c44af703150f8bb441
MARCO <37539338+MattiaFang@users.noreply.github.com>
2021-07-13 17:42:24 +0800

    base files
    基本electron应用，实现自动刷新页面

---
+++ main.js
@@ -0,0 +1,14 @@
+const {app,BrowserWindow} = require('electron')
+
+// 热加载
+const reloader = require('electron-reloader')
+reloader(module)
+
+// 监听初始化完成得到生命周期
+app.on('ready',()=>{
+    const mainWindow = new BrowserWindow({
+        width:500,
+        height:500
+    })
+    mainWindow.loadFile('./index.html')
+})
